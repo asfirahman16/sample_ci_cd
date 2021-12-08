1ccbdca5279fac5467814ccc5930735dd916a681
asfirahman16 <asfirahman@iut-dhaka.edu>
2021-12-08 12:32:43 +0600

    appjs update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send('Welcome Azure')
+    res.send('Welcome to my site!!')
 })
 
 app.listen(8080, ()=>{
